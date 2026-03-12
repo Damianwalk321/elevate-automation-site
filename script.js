b5b3d1a0e567bcc8689e33b12070dcfed7f3445d
Elevateautomation <Elevategpmarketing@gmail.com>
2026-03-12 17:54:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://teixblbxkoershwgqpym.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlaXhibGJ4a29lcnNod2dxcHltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODUzMDMsImV4cCI6MjA4ODY2MTMwM30.wxt9zjKhsBuflaFZZT9awZiwckRzYkEl-OLm_4q8qF4";
 const CREATE_CHECKOUT_SESSION_URL = "https://teixblbxkoershwgqpym.supabase.co/functions/v1/create-checkout-session";
 
 async function insertIntoSupabase(table, payload) {
