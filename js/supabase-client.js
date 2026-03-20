f4a3525ef201aec6f8c1ebda9e83c6127ea4a17f
Elevateautomation <Elevategpmarketing@gmail.com>
2026-03-20 17:25:55 -0600

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -5,7 +5,6 @@
     console.error("Supabase CDN client is not loaded.");
     return;
   }
-
   const SUPABASE_URL = "https://teixblbxkoershwgqpym.supabase.co";
   const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlaXhibGJ4a29lcnNod2dxcHltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODUzMDMsImV4cCI6MjA4ODY2MTMwM30.wxt9zjKhsBuflaFZZT9awZiwckRzYkEl-OLm_4q8qF4";
 
@@ -18,6 +17,9 @@
     console.error("Supabase URL or anon key is missing in /js/supabase-client.js");
     return;
   }
+
+  window.__ELEVATE_SUPABASE_URL = SUPABASE_URL;
+  window.__ELEVATE_SUPABASE_ANON_KEY = SUPABASE_ANON_KEY;
 
   window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
